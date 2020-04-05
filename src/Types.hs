6933195f2fd21df62529324619203026a38f2366
Joshua-Gordon <joshua.sam.gordon@gmail.com>
2020-04-05 12:16:28 -0400

    Player update

--- src/Types.hs
+++ src/Types.hs
@@ -9,3 +9,14 @@ type GridState square a = State (M.Map (Int,Int) square) a
 data World = World {
     map :: GridState Tile [[Tile]]
 }
+type Inventory = M.Map String Int
+
+data Player = Player {
+    player_x :: Int,
+    player_y :: Int,
+    player_hull :: Int,
+    player_heat :: Int,
+    heat_thresh :: Int,
+    player_inv :: Inventory
+}
+
