11ff9536144023eb88a1f9cc4985c51316ca3eba
16kuhnb@gmail.com <16kuhnb@gmail.com>
2020-04-05 17:19:45 -0400

    waiting

--- src/Events.hs
+++ src/Events.hs
@@ -3,6 +3,7 @@ import Graphics.Gloss
 import Graphics.Gloss.Interface.Pure.Game
 import Types
 import Player
+import Tick
 
 handleEvent :: Event -> World -> World
 handleEvent (EventKey key ks' modifiers _) w = let ks = (ks' == Down)
@@ -11,7 +12,7 @@ handleEvent (EventKey key ks' modifiers _) w = let ks = (ks' == Down)
                                                  Char 'a' -> w{left=ks}
                                                  Char 's' -> w{down=ks}
                                                  Char 'd' -> w{right=ks}
-                                                 Char ' ' -> if ks then w{player = tick_heat . heat_damage $ player w} else w
+                                                 (SpecialKey KeySpace) -> if ks then doHeat w else w
                                                  _ -> w
 handleEvent _ w = w
 
