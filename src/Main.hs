4aea4d634ce729a8f375f2b189889bb125762aeb
Ember Arlynx <ember@lunar.town>
2020-04-05 14:57:06 -0700

    playIO

--- src/Main.hs
+++ src/Main.hs
@@ -5,7 +5,7 @@ import World
 import Inventory
 import Render
 import Tick
-import Graphics.Gloss.Interface.Pure.Game
+import Graphics.Gloss.Interface.IO.Game
 import Events
 import Loader
 import Menu
@@ -15,7 +15,10 @@ main = do
       loadedAssets <- loadAssets
       let w=new_world{assets=loadedAssets}
       let m = initialMenu loadedAssets w
-      play FullScreen black 30 m renderMenu handleMenuEvent updateMenu
+      playIO FullScreen black 30 m 
+                              (return . renderMenu)
+                        (\event world -> return $ handleMenuEvent event world)
+                        (\time  world -> return $ updateMenu     time  world)
       --play FullScreen black 30 w renderWorld handleEvent tickWorld
 
 
